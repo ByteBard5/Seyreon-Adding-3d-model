8384911d9209eaa9fb80df9b9225f141b7000ae5
Yash Maheshwari <maheshwariy930@gmail.com>
2025-07-25 20:13:30 +0530

    Updated web app URL

--- src/Pages/ClientPortal/DashboardPage/clientNotes.js
+++ src/Pages/ClientPortal/DashboardPage/clientNotes.js
@@ -1,6 +1,6 @@
 //use  to Post and Get the data from google sheet to website for the last 2 sections of Client dashboard, (Showing previous notes, and to new notes using a text block)
 const SHEET_ENDPOINT =
-  "https://script.google.com/macros/s/AKfycbzJ2UGWOQeQZOTgWfpjetESs_QYz5eG-ggIjfAGotDML4uCPX-ogZrHVydNRB9NOSo1/exec";
+  "https://script.google.com/macros/s/AKfycbx_WY1fGGTmsgxgRySbJMV2RyALS3FEi3IhTqVPaQGIL0mqXRCts7btDR1SPV8ijcey/exec";
 
 // ✅ Append a new note for a client
 export async function appendClientNote(clientId, noteText) {
